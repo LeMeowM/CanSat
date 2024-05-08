d2de95593e7e228601b233a6b847f27eecc94003
Hugo Noublanche <108581111+LeMeowM@users.noreply.github.com>
2024-05-08 15:26:21 +0200

    Update Writing.py
    added logging

--- Writing.py
+++ Writing.py
@@ -11,8 +11,10 @@ import digitalio
 import adafruit_rfm69
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
-
-
+import logging
+logger = getLogger(__name__)
+logger.info("-------Prelaunch Initialisation------")
+logger.info("Begin initialisation of radio")
 # Define radio parameters.
 RADIO_FREQ_MHZ = 434.0  # Frequency of the radio in Mhz. Must match your
 # module! Can be a value like 915.0, 433.0, etc.
@@ -21,7 +23,7 @@ RADIO_FREQ_MHZ = 434.0  # Frequency of the radio in Mhz. Must match your
 CS = digitalio.DigitalInOut(board.D4)  # SPI0 CE0 which is also DPI D4
 RESET = digitalio.DigitalInOut(board.D1)  # GPIO 5, DPI D1
 
-
+logger.info("Begin initialisation of SPI")
 # Initialize SPI bus.
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
@@ -31,30 +33,42 @@ rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ)
 # Optionally set an encryption key (16 byte AES key). MUST match both
 # on the transmitter and receiver (or be set to None to disable/the default).
 rfm69.encryption_key = b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"  # TODO: fix keys
+logger.info("Radio initialised successfully")
 
+logger.info("initialising accelerometer")
 i2c = busio.I2C(board.SCL, board.SDA)
 adx = adafruit_adxl34x.ADXL345(i2c)
+logger.info("Accelerometer initialised")
 
-
+logger.info("Initialising GPS")
 # GPSDSocket creates a GPSD socket connection & request/retrieve GPSD output.
 gps_socket = agps3.GPSDSocket()
 # DataStream unpacks the streamed gpsd data into python dictionaries.
 data_stream = agps3.DataStream()
 gps_socket.connect()
 gps_socket.watch()
-
-# Output file name
-OUTPUT = str(time.time) + ".csv"
+logger.info("GPS initialised")
 
 
-stop_threads = False
 
-main_thread = True
+
+logger.info("Initialising camera")
+
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 encoder = H264Encoder(bitrate=10000000)
 output = "test.h264"
+
+logger.info("Camera initialised")
+
+# Output file name
+OUTPUT = str(time.time) + ".csv"
+
+stop_threads = False
+main_thread = True
+
+logger.info("------Initialisation Complete-------")
 
 
 def receiver():
