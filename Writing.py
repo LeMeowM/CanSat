c33787a59d544136d15b331cc17b54c808e4c1fb
Hugo Noublanche <108581111+LeMeowM@users.noreply.github.com>
2024-05-09 12:41:46 +0200

    Update Writing.py
    fixing debugger

--- Writing.py
+++ Writing.py
@@ -16,6 +16,19 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
 logger.info("-------Prelaunch Initialisation------")
 logger.info("Begin initialisation of radio")
 # Define radio parameters.
