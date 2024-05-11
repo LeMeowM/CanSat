37e55163341cca781cf87270a42da62cc70db6cc
Hugo Noublanche <108581111+LeMeowM@users.noreply.github.com>
2024-05-11 07:31:24 +0200

    Update OfflineWrite.py
    parsing all of the code safely

--- OfflineWrite.py
+++ OfflineWrite.py
@@ -3,39 +3,38 @@
 from gps3 import agps3
 import time
 import csv
-import threading
 import adafruit_adxl34x
 import board
 import busio
 import digitalio
+import traceback
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 import logging
 import sys
 
-
+# creating logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-# create console handler and set level to debug
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-
-# create formatter
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-# add formatter to ch
 ch.setFormatter(formatter)
-
-# add ch to logger
 logger.addHandler(ch)
 
+logger.warning("Make sure you run\nsudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock\nbefore running this code if its your first time after launch!")
+
+fh = logging.FileHandler('spam.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+# prelaunch initialisation
 logger.info("-------Prelaunch Initialisation------")
+initstart = time.time()
 all = False
 if "--a" in sys.argv:
     all = True
-
-
 if all or "--acc" in sys.argv:
     logger.info("initialising accelerometer")
     i2c = busio.I2C(board.SCL, board.SDA)
@@ -50,17 +49,17 @@ if all or "--gnss" in sys.argv:
     gps_socket.connect()
     gps_socket.watch()
     logger.info("GPS initialised")
+else:
+    raise NotImplementedError
 cameraOn = False
 if all or "--cam" in sys.argv:
     cameraOn = True
     logger.info("Initialising camera")
-
     picam2 = Picamera2()
     video_config = picam2.create_video_configuration()
     picam2.configure(video_config)
     encoder = H264Encoder(bitrate=10000000)
     output = "test.h264"
-
     logger.info("Camera initialised")
 
 # Output file name
@@ -69,13 +68,23 @@ OUTPUT = str(time.time()) + ".csv"
 stop_threads = False
 main_thread = True
 
-logger.info("------Initialisation Complete-------")
-start = time.time()
 PERIOD_OF_TIME = 300  # 5min
+if "--time" in sys.argv:
+    try:
+        indexplusone = sys.argv[sys.argv.index("--time")+1]
+        PERIOD_OF_TIME = int(indexplusone)
+        logger.info("Mission length set to {PERIOD_OF_TIME} seconds.")
+    except IndexError:
+        logger.warning("Argument order is wrong, time set ignored. Mission length is set to 300 seconds.")
+    except ValueError:
+        logger.warning("{indexplusone} is not a number. Mission length is set to 300 seconds.")
+logger.info("------Initialisation Complete-------")
+logger.debug(f"Initialisation took {time.time()-initstart} seconds")
 
+start = time.time()
 
 def write_to_file():
-    with open(OUTPUT, "w") as output_file:
+    with open(OUTPUT, "a") as output_file:
         write = csv.writer(output_file, lineterminator="\n")
         write.writerow("time", "lon", "lat", "speed", "accx", "accy", "accz")
         while True:
@@ -92,12 +101,56 @@ def write_to_file():
                             float(data_stream.speed),
                             float(accx),
                             float(accy),
-                            float(accz),
+                            float(accz)
+                        ]   
+                    else:
+                        array = [
+                            time.time(),
+                            "n/a",
+                            "n/a",
+                            "n/a",
+                            "n/a",
+                            float(accx),
+                            float(accy),
+                            float(accz)
                         ]
-                        write.writerow(array)
-                        time.sleep(0.2)
+                    write.writerow(array)
+                    time.sleep(0.2)          
             if time.time() > start + PERIOD_OF_TIME:
-                break
+                return "mission successful"
 
+def write_to_file_safe():
+    with open(OUTPUT, "a") as output_file:
+        write = csv.writer(output_file, lineterminator="\n")
+        write.writerow("time", "accx", "accy", "accz")
+        while True:
+                accx, accy, accz = adx.acceleration
+                array = [
+                    time.time(),
+                    "n/a",
+                    "n/a",
+                    "n/a",
+                    "n/a",
+                    float(accx),
+                    float(accy),
+                    float(accz),
+                ]
+                write.writerow(array)
+                time.sleep(0.5)
+                if time.time() > start + PERIOD_OF_TIME:
+                    return "mission successful"
 
-write_to_file()
+logger.info("Starting mission")
+try:
+    logger.info(write_to_file())
+except Exception:
+    logger.warning("Write failed, falling back to safer write.")
+    # if this fails all goes to shit lol, might as well traceback
+    try:
+        logger.info(write_to_file_safe())
+    except Exception:
+        logger.exception("its jover")
+else:
+    logger.info("Mission completed successfully, and without errors.")
+finally:
+    logger.info(f"EOF reached at {time.asctime()}")
